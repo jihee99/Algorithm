4f474788fada0187d3682dd6e08cf21e0da2f413
jihee <63203622+jihee99@users.noreply.github.com>
2023-06-18 14:46:17 +0900

    Initial commit

---
+++ src/main/java/class1/q1152.java
@@ -0,0 +1,23 @@
+package class1;
+
+import java.util.*;
+
+public class q1152 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String trimStr = str.trim();
+        if(trimStr.length()>0){
+            String[] arr = str.trim().split(" ");
+            System.out.println(arr.length);
+        }else{
+            System.out.println("0");
+        }
+//        System.out.print(str.trim());
+//        String[] arr = str.trim().split(" ");
+//
+//        System.out.println(arr.length);
+    }
+}
