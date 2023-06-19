062c036905ffcf32d1fa34977c2cec6b331efa91
jihee <63203622+jihee99@users.noreply.github.com>
2023-06-19 16:35:02 +0900

    Q2438

---
+++ src/main/java/class1/q2438.java
@@ -0,0 +1,19 @@
+package class1;
+
+import java.util.*;
+
+public class q2438 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int line = sc.nextInt();
+
+        for(int i=1; i<-line; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
