997685365e0b13b0cd3666c61d28b57ee20e7550
jihee <oosoojh31@gmail.com>
2023-06-28 14:23:56 +0900

    q10171

---
+++ src/main/java/class1/q10171.java
@@ -0,0 +1,15 @@
+package class1;
+
+public class q10171 {
+//\    /\
+// )  ( ')
+//(  /  )
+// \(__)|
+
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
