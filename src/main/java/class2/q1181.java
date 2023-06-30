54cb54b40755cfb4500eee96e33f362eabcff420
jihee <oosoojh31@gmail.com>
2023-06-30 16:15:02 +0900

    q1181

---
+++ src/main/java/class2/q1181.java
@@ -0,0 +1,16 @@
+package class2;
+
+import java.io.*;
+
+public class q1181 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int c = Integer.parseInt(br.readLine());
+
+
+
+
+    }
+}
