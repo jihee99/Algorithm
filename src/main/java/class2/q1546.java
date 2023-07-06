d5a011255ca0f5bfc9f540317c26d31d06e65392
jihee <oosoojh31@gmail.com>
2023-07-06 15:55:52 +0900

    q1546 - 수정

--- src/main/java/class2/q1546.java
+++ src/main/java/class2/q1546.java
@@ -10,20 +10,26 @@ public class q1546 {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+//        int[] arr = new int[N];
         int max = -1;
+
+        double result =0;
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            if(max<arr[i]) max = arr[i];
+            int num = Integer.parseInt(st.nextToken());
+            if(max<num) max = num;
+            result += num;
+//            arr[i] = Integer.parseInt(st.nextToken());
+//            if(max<arr[i]) max = arr[i];
+
         }
 
-        double result =0;
-        for (double n : arr) {
-//            result += n / max * 100;
-            result += n;
-        }
+//        double result =0;
+//        for (double n : arr) {
+////            result += n / max * 100;
+//            result += n;
+//        }
         bw.write(String.valueOf((result/ max * 100) /  N ));
         bw.flush();
         bw.close();
