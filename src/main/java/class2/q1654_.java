7abdd16440fc5d5d5c6ff831f74573c5602b9099
손지희 <wlwlhee@ejbt.co.kr>
2023-07-08 15:53:04 +0900

    1654 -

--- src/main/java/class2/q1654_.java
+++ src/main/java/class2/q1654_.java
@@ -19,7 +19,7 @@ public class q1654_ {
         long max = -1;
         for(int i=0; i<K; i++){
             arr[i] = Long.parseLong(br.readLine());
-            if(arr[i] > max) max = arr[i];
+            max = Math.max(max, arr[i]);
         }
         Arrays.sort(arr);
         for(long i : arr){
@@ -27,7 +27,7 @@ public class q1654_ {
         }
 
 
-        long result = searchBinary(arr, max, K);
+        long result = searchBinary(arr, max, N);
 
         System.out.println(result);
         // 최대길이의 랜선을 찾는 함수 호출
@@ -39,28 +39,40 @@ public class q1654_ {
 
     public static long searchBinary(long[] arr, long max, int N){
 
-        long left = 1;
-        long right = max;
-        while(left <= right){
-            long mid = (left + right) / 2;
+        long min = 1;
+        long half = 0;
+
+        while(min <= max){
+            half = (min + max) / 2;
+            int count = 0;
 
             System.out.println("------------");
-            System.out.println(mid);
+            System.out.println(half);
             int sum = 0;
 
+
             for(long i : arr){
-                sum += i/mid;
-                System.out.println(sum);
+                count += i/half;
+                System.out.println(count);
             }
 
             System.out.println("   ");
 
-            if(sum < N) right = mid-1;
-            else if(sum >= N) left = mid+1;
+            // 원하는 랜선 갯수 보다 잘라진 랜선 수가 적을경우
+            // 하나의 랜선 길이가 길어서 길이를 더 짧게 만들어야 함
+            // half보다 아래의 수.
+            if(count < N) {
+                max = half-1;
+            }
+            // 원하는 랜선 갯수 보다 잘라진 랜선 수가 많을경우
+            // 하나의 랜선 길이가 짧아서 더 길게 만들어야 함
+            // half보다 위의 수에 있음.
+            else {
+                min = half+1;
+            }
 
         }
 
-        return left;
-
+        return (max+min)/2;
     }
 }
