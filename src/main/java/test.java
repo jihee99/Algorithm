66f64f40062bba76c65c42d08e512ace6ffdbdc7
jihee <oosoojh31@gmail.com>
2023-07-19 08:03:39 +0900

    q1032

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,5 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class test {
 
@@ -43,27 +42,76 @@ public class test {
 //    Q1546
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int count = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        bw.write(String.valueOf(A+B+C));
+        bw.flush();
+        bw.close();
+
+//        int A = Integer.parseInt(br.readLine());
+//        int B = Integer.parseInt(br.readLine());
+//
+//        bw.write(String.valueOf(A*(B%10)));
+//        bw.newLine();
+//
+//        bw.write(String.valueOf(A*((B%100)/10)));
+//        bw.newLine();
+//
+//        bw.write(String.valueOf(A*(B/100)));
+//        bw.newLine();
+//
+//        bw.write(String.valueOf(A*B));
+//        bw.flush();
+//        bw.close();
 
 
-        double[] arr = new double[count];
-
-        for(int i=0; i<count; i++){
-            arr[i] = Double.parseDouble(st.nextToken());
-        }
-        Arrays.sort(arr);
-
-        double max = arr[arr.length - 1];
-
-        double ave = 0;
-        for(double d :arr){
-            ave += (double) d/max*100;
-        }
-
-        System.out.println();
-        System.out.println( ave/count);
+//        int count = Integer.parseInt(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//        int A = Integer.parseInt(st.nextToken());
+//        int B = Integer.parseInt(st.nextToken());
+//        int C = Integer.parseInt(st.nextToken());
+//
+//        //둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C
+//        bw.write(String.valueOf((A+B)%C));
+//        bw.newLine();
+//
+//        bw.write(String.valueOf(((A%C) + (B%C))%C));
+//        bw.newLine();
+//
+//        bw.write(String.valueOf((A*B)%C));
+//        bw.newLine();
+//
+//        bw.write(String.valueOf(((A%C) * (B%C))%C));
+//        bw.newLine();
+//
+//
+//        bw.flush();
+//        bw.close();
+//
+//
+//        double[] arr = new double[count];
+//
+//        for(int i=0; i<count; i++){
+//            arr[i] = Double.parseDouble(st.nextToken());
+//        }
+//        Arrays.sort(arr);
+//
+//        double max = arr[arr.length - 1];
+//
+//        double ave = 0;
+//        for(double d :arr){
+//            ave += (double) d/max*100;
+//        }
+//
+//        System.out.println();
+//        System.out.println( ave/count);
 
     }
 
