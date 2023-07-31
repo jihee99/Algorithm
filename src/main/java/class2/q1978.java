71dcec07cdc69b0df9801d5f6356b60a07948c6d
jihee <oosoojh31@gmail.com>
2023-07-31 11:09:06 +0900

    q1978수정

--- src/main/java/class2/q1978.java
+++ src/main/java/class2/q1978.java
@@ -24,7 +24,7 @@ public class q1978 {
 
     public static boolean isPrime(int num){
         if(num<2) return false;
-        for(int i=2; i<num; i++){
+        for(int i=2; i*i<num; i++){
             if(num%i==0) return false;
         }
         return true;
