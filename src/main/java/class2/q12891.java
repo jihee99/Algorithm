efb1259a67013b7f1e1f8a2c792398ac96cd854d
jihee <oosoojh31@gmail.com>
2023-08-03 13:35:04 +0900

    q12891 - timeout

--- src/main/java/class2/q12891.java
+++ src/main/java/class2/q12891.java
@@ -3,13 +3,14 @@ package class2;
 //9 8
 //CCTGGATTG
 //2 0 1 1
-//
+// -> 0
 //4 2
 //GATA
 //1 0 0 1
-
+// ->2
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class q12891 {
@@ -24,36 +25,60 @@ public class q12891 {
         int P = Integer.parseInt(st.nextToken());
 
         String dna = br.readLine();
+
+        char[] chkDna = new char[S];
+        int[] A = new int[4];
+
+
+        int count = 0;
+
         st = new StringTokenizer(br.readLine());
 
-        int[] rules = new int[P];
-
-        int chkA = Integer.parseInt(st.nextToken());
-        int chkC = Integer.parseInt(st.nextToken());
-        int chkG = Integer.parseInt(st.nextToken());
-        int chkT = Integer.parseInt(st.nextToken());
-
         for(int i=0; i<4; i++){
-//            CCTGGATTG
-            rules[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
         for(int i=0; i<S-P+1; i++){
+            int[] chkArr = new int[4];
             int end = i+P;
             String chkStr = dna.substring(i, end);
 
+            for(int j=0; j<chkStr.length(); j++){
+                char ch = chkStr.charAt(j);
+                switch (ch){
+                    case 'A':
+                        chkArr[0]++;
+                        break;
+                    case 'C':
+                        chkArr[1]++;
+                        break;
+                    case 'G':
+                        chkArr[2]++;
+                        break;
+                    case 'T':
+                        chkArr[3]++;
+                        break;
+                }
+            }
 
-//            for(int j=0; j<chkStr.length(); j++){
-//                char ch = chkStr.charAt(j);
-//                if (ch)
-//                if(ch == 'a')
-//                System.out.println(chkStr.charAt(j));
-//            }
-            System.out.println(chkStr);
+            if(pwdValidChk(A, chkArr)){
+                count++;
+            }
+
+//            System.out.println(chkStr);
         }
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
     }
 
-    public static int countChar(String str, char ch){
-        return str.length() - str.replace(String.valueOf(ch), "").length();
+    public static boolean pwdValidChk(int[] A, int[] my){
+        boolean pwdChk = false;
+        for(int i=0; i<4; i++){
+            if(A[i]==my[i]) pwdChk = true;
+            else pwdChk = false;
+        }
+        return pwdChk;
     }
 }
