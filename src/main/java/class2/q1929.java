5160fdd09a9d02786286584f48cf5c99bc5c88bb
jihee <oosoojh31@gmail.com>
2023-08-08 11:04:53 +0900

    q1929

--- src/main/java/class2/q1929.java
+++ src/main/java/class2/q1929.java
@@ -14,21 +14,38 @@ public class q1929 {
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
+        boolean arr[] = new boolean[N+1];
+        arr[0] = arr[1] = true;
+
+        int sqrt = (int)Math.sqrt(N);
+        for(int i=2; i<=sqrt; i++) {
+            for(int j=2; j<=N/i; j++) {
+                if(arr[i*j] == true) {
+                    continue;
+                }
+                else  {
+                    arr[i*j] = true;
+                }
+            }
+        }
+
+
         for(int i=M; i<=N; i++) {
-            if(isPrime(i)){
+            if(arr[i] == false) {
                 bw.write(String.valueOf(i));
                 bw.newLine();
             }
         }
+
         bw.flush();
         bw.close();
     }
-    public static boolean isPrime(int num){
-
-        if(num<2) return false;
-        for(int i=2; i<=num/2;i++){
-            if(num%i == 0) return false;
-        }
-        return true;
-    }
+//    public static boolean isPrime(int num){
+//
+//        if(num<2) return false;
+//        for(int i=2; i<=num/2;i++){
+//            if(num%i == 0) return false;
+//        }
+//        return true;
+//    }
 }
