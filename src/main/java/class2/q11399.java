090c09a94f6819410849170cdd492d313eed7afe
jihee99 <oosoojh31@gmail.com>
2023-09-11 18:42:32 +0900

    q11399

--- src/main/java/class2/q11399.java
+++ src/main/java/class2/q11399.java
@@ -1,6 +1,7 @@
 package class2;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class q11399 {
@@ -12,20 +13,29 @@ public class q11399 {
 
         int[] intArr = new int[N];
 
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+        int[] sumArr = new int[N];
+
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++){
             intArr[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
 
+        Arrays.sort(intArr);
+
+        sumArr[0] = intArr[0];
+
         for(int i=1; i<N; i++){
-            int current = intArr[i];
-            for(int j=0; j<1; j++){
-                if(intArr[j] > current){
-                    int temp =
-                }
-            }
+            sumArr[i] = sumArr[i-1] + intArr[i];
         }
 
+        int result = 0;
 
+        for(int i=0; i<N; i++){
+            result += sumArr[i];
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
     }
 }
