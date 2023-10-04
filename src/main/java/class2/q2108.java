2036132aa870b335aa7adc1a0bfa33dc772b61fc
jihee <oosoojh31@gmail.com>
2023-10-04 16:28:24 +0900

    q2108 수정중

---
+++ src/main/java/class2/q2108.java
@@ -0,0 +1,20 @@
+package class2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class q2108 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++){
+			
+		}
+	}
+}
