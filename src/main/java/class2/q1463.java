0a0bbde362e5af9e29e2e8b071ce870484eace5e
jihee99 <oosoojh31@gmail.com>
2023-10-15 17:05:55 +0900

    q1463 풀이 완

--- src/main/java/class2/q1463.java
+++ src/main/java/class2/q1463.java
@@ -10,16 +10,17 @@ public class q1463 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        while(N > 1){
-            if(N%3 == 0) N = N/3;
-            else if((N-1) % 3 == 0) N = N-1;
-            else if(N%2 == 0) N = N/3;
-            else N = N - 1;
+        int[] dp = new int[N+1];
 
-            count++;
+        dp[0] = dp[1] = 0;
+
+        for(int i=2; i<=N; i++){
+            dp[i] = dp[i-1]+1;
+            if(i%2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if(i%3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
         }
-        bw.write(String.valueOf(count));
+
+        bw.write(String.valueOf(dp[N]));
         bw.flush();
         bw.close();
     }
