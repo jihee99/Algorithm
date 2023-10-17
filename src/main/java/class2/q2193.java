2f68b63cb4ffbda7118c616be1a71f50fab7af49
jihee <oosoojh31@gmail.com>
2023-10-17 16:10:30 +0900

    q2193 풀이완
    -int 타입 에러 long으로 변경

--- src/main/java/class2/q2193.java
+++ src/main/java/class2/q2193.java
@@ -13,10 +13,10 @@ public class q2193 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int count = Integer.parseInt(br.readLine());
-
+		long[][] arr = new long[count+1][2];
 
 		arr[1][0] = 0;
-		arr[1][1] = 1;		long[][] arr = new long[count+1][2];
+		arr[1][1] = 1;
 		for(int i=2; i<=count; i++){
 			arr[i][0] = arr[i-1][0] + arr[i-1][1];
 			arr[i][1] = arr[i-1][0];
