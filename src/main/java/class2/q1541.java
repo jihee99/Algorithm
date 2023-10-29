b6e5364a7c7150f21730ded8fc6d8feeea8e145e
jihee99 <oosoojh31@gmail.com>
2023-10-29 15:13:33 +0900

    1541 풀이 완

--- src/main/java/class2/q1541.java
+++ src/main/java/class2/q1541.java
@@ -1,8 +1,36 @@
 package class2;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class q1541 {
-	public static void main(String[] agrs){
+	public static void main(String[] agrs) throws IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		// 초기값 설정
+		int sum = Integer.MAX_VALUE;
+
+		StringTokenizer sub = new StringTokenizer(br.readLine(),"-");
+
+		while(sub.hasMoreTokens()){
+			StringTokenizer add = new StringTokenizer(sub.nextToken(), "+");
+
+			int temp = 0;
+
+			while(add.hasMoreTokens()){
+				temp += Integer.parseInt(add.nextToken());
+			}
+
+			if(sum == Integer.MAX_VALUE){
+				sum = temp;
+			}else{
+				sum -= temp;
+			}
+		}
+		bw.write(String.valueOf(sum));
+		bw.flush();
+		bw.close();
 	}
 }
