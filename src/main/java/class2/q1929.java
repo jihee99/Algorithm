988b101f506d07d2e7c6b557fbb3d04d27293547
jihee99 <oosoojh31@gmail.com>
2023-10-29 15:42:58 +0900

    1929 - 에라토스테네스의 체

--- src/main/java/class2/q1929.java
+++ src/main/java/class2/q1929.java
@@ -11,41 +11,65 @@ public class q1929 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
 
-        boolean arr[] = new boolean[N+1];
+        boolean[] arr = new boolean[end+1];
+
+        // true 소수아님, false 소수
+        Arrays.fill(arr, false);
+
         arr[0] = arr[1] = true;
 
-        int sqrt = (int)Math.sqrt(N);
-        for(int i=2; i<=sqrt; i++) {
-            for(int j=2; j<=N/i; j++) {
-                if(arr[i*j] == true) {
-                    continue;
-                }
-                else  {
-                    arr[i*j] = true;
-                }
+        for(int i=2; i<arr.length; i++){
+            if(arr[i]) continue;
+            for(int j=i+i; j<arr.length; j += i){
+                arr[j] = true;
             }
         }
 
-
-        for(int i=M; i<=N; i++) {
-            if(arr[i] == false) {
-                bw.write(String.valueOf(i));
-                bw.newLine();
-            }
+        StringBuilder sb = new StringBuilder();
+        for(int i=start; i<=end; i++){
+            if(!arr[i]) sb.append(i).append("\n");
         }
 
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
-    }
-//    public static boolean isPrime(int num){
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //
-//        if(num<2) return false;
-//        for(int i=2; i<=num/2;i++){
-//            if(num%i == 0) return false;
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int M = Integer.parseInt(st.nextToken());
+//        int N = Integer.parseInt(st.nextToken());
+//
+//        boolean arr[] = new boolean[N+1];
+//        arr[0] = arr[1] = true;
+//
+//        int sqrt = (int)Math.sqrt(N);
+//        for(int i=2; i<=sqrt; i++) {
+//            for(int j=2; j<=N/i; j++) {
+//                if(arr[i*j] == true) {
+//                    continue;
+//                }
+//                else  {
+//                    arr[i*j] = true;
+//                }
+//            }
 //        }
-//        return true;
-//    }
+//
+//
+//        for(int i=M; i<=N; i++) {
+//            if(arr[i] == false) {
+//                bw.write(String.valueOf(i));
+//                bw.newLine();
+//            }
+//        }
+//
+//        bw.flush();
+//        bw.close();
+    }
+
 }
