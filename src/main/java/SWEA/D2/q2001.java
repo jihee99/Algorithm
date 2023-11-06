cc6c4d7c0b4b082e321f0c9e89a91e0f117ce06e
jihee <oosoojh31@gmail.com>
2023-11-06 15:48:07 +0900

    2001 - 누적합 풀이 완료

--- src/main/java/SWEA/D2/q2001.java
+++ src/main/java/SWEA/D2/q2001.java
@@ -34,50 +34,31 @@ public class q2001 {
 
 
 			// 누적합
-			// for(int i=1; i<N+1; i++){
-			// 	for(int j=1; j<N+1; j++){
-			// 		preSum[i][j] = board[i-1][j-1] + preSum[i][j-1] + preSum[i-1][j] - preSum[i-1][j-1];
-			// 	}
-			// }
-			//
-			// int max = Integer.MIN_VALUE;
-			// for(int i=1; i<=N-M+1; i++){
-			// 	for(int j=1; j<=N-M+1; j++){
-			// 		int sum = board[i][j] - preSum[i-1][j] - preSum[i][j-1] + preSum[i-1][j-1];
-			// 		max  = Math.max(sum, max);
-			// 	}
-			// }
-
-
-			int max = Integer.MIN_VALUE;
-			for(int i=0; i<N-M+1; i++){
-				for(int j=0; j<N-M+1; j++){
-					int sum = 0;
-					for(int k=i; k<i+M; k++){
-						for(int l=j; l<j+M; l++){
-							sum += board[k][l];
-						}
-					}
-					max = Math.max(max, sum);
+			for(int i=1; i<N+1; i++){
+				for(int j=1; j<N+1; j++){
+					preSum[i][j] = board[i-1][j-1] + preSum[i][j-1] + preSum[i-1][j] - preSum[i-1][j-1];
 				}
 			}
 
-			// int[][] board = new int[m][m];
-			// int[][] prefixSum = new int[m + 1][m + 1];
+			int max = Integer.MIN_VALUE;
+			for(int i=M; i<=N; i++){
+				for(int j=M; j<=N; j++){
+					int sum = preSum[i][j] - preSum[i-M][j] - preSum[i][j-M] + preSum[i-M][j-M];
+					max  = Math.max(sum, max);
+				}
+			}
+
+
 			// int max = Integer.MIN_VALUE;
-			// for (int i = 0; i < m; i++) {
-			// 	board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			// }
-			// for (int i = 1; i < m + 1; i++) {
-			// 	for (int j = 1; j < m + 1; j++) {
-			// 		prefixSum[i][j] = board[i - 1][j - 1] + prefixSum[i][j - 1] + prefixSum[i - 1][j] - prefixSum[i -1][j - 1];
-			// 	}
-			// }
-			//
-			// for (int i = 1; i <= m - n + 1; i++) {
-			// 	for (int j = 1; j <= m - n + 1; j++) {
-			// 		int sum = prefixSum[n + i - 1][n + j - 1] - prefixSum[n + i - 1][j - 1] - prefixSum[i - 1][n + j - 1] + prefixSum[i - 1][j - 1];
-			// 		max = Math.max(sum, max);
+			// for(int i=0; i<N-M+1; i++){
+			// 	for(int j=0; j<N-M+1; j++){
+			// 		int sum = 0;
+			// 		for(int k=i; k<i+M; k++){
+			// 			for(int l=j; l<j+M; l++){
+			// 				sum += board[k][l];
+			// 			}
+			// 		}
+			// 		max = Math.max(max, sum);
 			// 	}
 			// }
 
